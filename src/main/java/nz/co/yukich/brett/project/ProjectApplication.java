7969211ad0837926b271621aba6931e93d0528fd
Brett Yukich <brett.yukich@fostermoore.com>
2018-03-28 13:40:06 +1300

    Getting started with Spring Boot + MithrilJS

---
+++ src/main/java/nz/co/yukich/brett/project/ProjectApplication.java
@@ -0,0 +1,12 @@
+package nz.co.yukich.brett.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+}
---
+++ src/main/java/nz/co/yukich/brett/project/ProjectController.java
@@ -0,0 +1,7 @@
+package nz.co.yukich.brett.project;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProjectController {
+}
