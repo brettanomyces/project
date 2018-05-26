51560b948ee10a7322de06ae3fd91d18f5efab77
Brett Yukich <brettyukich@gmail.com>
2018-05-26 20:51:14 +1200

    Move login view state into view

--- src/main/js/models/Auth.js
+++ src/main/js/models/Auth.js
@@ -1,65 +1,15 @@
 const m = require("mithril");
 
 const Auth = {
-  username: "",
-  validateUsername: false,
-  password: "",
-  validatePassword: false,
-  message: "",
-
-  reset: function() {
-    Auth.username = "";
-    Auth.validateUsername = false;
-    Auth.password = "";
-    Auth.validatePassword = false;
-    Auth.message = "";
-  },
-
-  setUsername: function (value) {
-    Auth.username = value
-  },
-
-  setValidateUsername: function(validate) {
-    Auth.validateUsername = validate
-  },
-
-  isUsernameValid: function() {
-    return Auth.username !== ""
-  },
-
-  setPassword: function (value) {
-    Auth.password = value
-  },
-
-  setValidatePassword: function(validate) {
-    Auth.validatePassword = validate
-  },
-
-  isPasswordValid: function() {
-    return Auth.password !== ""
-  },
-
-  canSubmit: function() {
-    return Auth.isUsernameValid() && Auth.isPasswordValid()
-  },
-
-  login: function () {
-    m.request({
+  login: function (username, password) {
+    return m.request({
       method: "POST",
       url: "/auth/login",
       data: {
-        "username": Auth.username,
-        "password": Auth.password,
+        "username": username,
+        "password": password
       }
     })
-      .then(function() {
-        Auth.reset();
-        m.route.set("home")
-      })
-      .catch(function (e) {
-        Auth.password = "";
-        Auth.message = e.message;
-      })
   },
 
   logout: function () {
--- src/main/js/views/Login.js
+++ src/main/js/views/Login.js
@@ -1,6 +1,50 @@
 const m = require("mithril");
 const Auth = require("../models/Auth");
 
+let state = {
+  username: "",
+  validateUsername: false,
+  password: "",
+  validatePassword: false,
+  message: "",
+
+  reset: function () {
+    state.username = "";
+    state.validateUsername = false;
+    state.password = "";
+    state.validatePassword = false;
+    state.message = "";
+  },
+
+  setUsername: function (value) {
+    state.username = value
+  },
+
+  setValidateUsername: function (validate) {
+    state.validateUsername = validate
+  },
+
+  isUsernameValid: function () {
+    return state.username !== ""
+  },
+
+  setPassword: function (value) {
+    state.password = value
+  },
+
+  setValidatePassword: function (validate) {
+    state.validatePassword = validate
+  },
+
+  isPasswordValid: function () {
+    return state.password !== ""
+  },
+
+  canSubmit: function () {
+    return state.isUsernameValid() && state.isPasswordValid()
+  },
+};
+
 module.exports = {
   view: function () {
     return m("section.section",
@@ -11,13 +55,19 @@ module.exports = {
               m("h1.title.is-1",
                 "Project"
               ),
-              Auth.message !== "" ? m(".notification.is-warning", Auth.message) : null,
+              state.message !== "" ? m(".notification.is-warning", state.message) : null,
               m("form", {
-                onsubmit: function (e) {
-                  e.preventDefault();
-                  Auth.login();
-                }
-              }, [
+                  onsubmit: function (event) {
+                    event.preventDefault();
+                    Auth.login(state.username, state.password).then(function () {
+                      state.reset();
+                      m.route.set("/home");
+                    }).catch(function (error) {
+                      state.password = "";
+                      state.message = error.message;
+                    })
+                  }
+                }, [
                   m(".field",
                     [
                       m("label.label",
@@ -25,10 +75,12 @@ module.exports = {
                       ),
                       m(".control",
                         m("input.input[type='text']", {
-                          value: Auth.username,
-                          class: Auth.validateUsername && !Auth.isUsernameValid() ? "is-danger" : "",
-                          oninput: m.withAttr("value", Auth.setUsername),
-                          onfocusout: function () { Auth.setValidateUsername(true) }
+                          value: state.username,
+                          class: state.validateUsername && !state.isUsernameValid() ? "is-danger" : "",
+                          oninput: m.withAttr("value", state.setUsername),
+                          onfocusout: function () {
+                            state.setValidateUsername(true)
+                          }
                         })
                       )
                     ]
@@ -40,10 +92,12 @@ module.exports = {
                       ),
                       m(".control",
                         m("input.input[type='password']", {
-                          value: Auth.password,
-                          class: Auth.validatePassword && !Auth.isPasswordValid() ? "is-danger" : "",
-                          oninput: m.withAttr("value", Auth.setPassword),
-                          onfocusout: function () { Auth.setValidatePassword(true) },
+                          value: state.password,
+                          class: state.validatePassword && !state.isPasswordValid() ? "is-danger" : "",
+                          oninput: m.withAttr("value", state.setPassword),
+                          onfocusout: function () {
+                            state.setValidatePassword(true)
+                          },
                         })
                       )
                     ]
@@ -52,7 +106,7 @@ module.exports = {
                     m(".control",
                       m("button.button.is-primary[type='submit']",
                         {
-                          disabled: !Auth.canSubmit()
+                          disabled: !state.canSubmit()
                         }, "Login")
                     )
                   )
