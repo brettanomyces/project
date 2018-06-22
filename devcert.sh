eb30e2a89879afd79bb17fd32bb9d0158263001e
Brett Yukich <brett.yukich@fostermoore.com>
2018-06-22 16:16:37 +1200

    TODO update script to create certificate authority

--- devcert.sh
+++ devcert.sh
@@ -16,9 +16,23 @@ fi
 echo "Generating keystore: ""$KEYSTORE"
 
 # private key and public certificate
-openssl req -x509 -out localhost.crt -keyout localhost.key \
-  -newkey rsa:2048 -nodes -sha256 \
-  -subj '/CN=localhost' -extensions EXT -config <(printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+# openssl req -x509 -out localhost.crt -keyout localhost.key \
+#   -newkey rsa:2048 -nodes -sha256 \
+#   -subj '/CN=localhost' -extensions EXT \
+#   -config <(printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth") \
+#   -extfile <(printf "authorityKeyIdentifier=keyid,issuer\nbasicConstraints=CA:FALSE\nsubjectAltName=@alt_names\n\n[alt_names]\nDNS.1 = localhost")
 
 # keystore
-openssl "$KEYSTORE_TYPE" -export -in localhost.crt -inkey localhost.key -out "$KEYSTORE" -passout pass:"$KEYSTORE_PASSWORD"
+# openssl "$KEYSTORE_TYPE" -export -in localhost.crt -inkey localhost.key -out "$KEYSTORE" -passout pass:"$KEYSTORE_PASSWORD"
+
+# generate private key
+openssl genrsa -out private-key.pem 2048
+
+# generate a certificate request
+openssl req -new -nodes -sha256 -key private-key.pem -out project-csr.pem -config devcert.conf
+
+# self-sign the certificate
+openssl x509 -req -sha256 -days 3650 -in project-csr.pem -signkey private-key.pem -out project-cert.pem -extfile devcert.conf -extensions v3_req
+
+# create keystore
+openssl "$KEYSTORE_TYPE" -export -in project-cert.pem -inkey private-key.pem -out "$KEYSTORE" -passout pass:"$KEYSTORE_PASSWORD"
